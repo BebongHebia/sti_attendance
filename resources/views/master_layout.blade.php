10583efc54965536f0aab997bcd6ec1a007107a2
BebongHebia <hebiadavinci@gmail.com>
2024-12-07 13:37:40 +0800

    footer fixed

--- resources/views/master_layout.blade.php
+++ resources/views/master_layout.blade.php
@@ -71,10 +71,10 @@
 
         <!-- Main Footer -->
         <footer class="main-footer">
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+            <strong>STI EVENT ATTENDANCE MANAGEMENT SYSTEM</strong>
             All rights reserved.
             <div class="float-right d-none d-sm-inline-block">
-                <b>Version</b> 3.2.0
+                <b>Version</b> 0.0.1
             </div>
         </footer>
     </div>
